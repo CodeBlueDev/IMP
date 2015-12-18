64fe2022501c9b991442c9b9ef699616a7953c6c
CodeBlue <codebluedev@gmail.com>
2015-12-18 08:54:15 -0700

    Add MenuStrip and ToolStrip to MainForm
    Add MenuStrip to hold commands available to user in the menu strip.
    
    Add ToolStrip to hold common commands that will be used by the user in
    the ToolStrip.
    
    Still needs to have the MenuStrip items and ToolStrip items added.

--- src/CodeBlueDev.Imp.WinForms/Forms/MainForm.Designer.cs
+++ src/CodeBlueDev.Imp.WinForms/Forms/MainForm.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.MenuStripMain = new System.Windows.Forms.MenuStrip();
+            this.ToolStripMain = new System.Windows.Forms.ToolStrip();
             this.SuspendLayout();
+            // 
+            // MenuStripMain
+            // 
+            this.MenuStripMain.Location = new System.Drawing.Point(0, 0);
+            this.MenuStripMain.Name = "MenuStripMain";
+            this.MenuStripMain.Size = new System.Drawing.Size(484, 24);
+            this.MenuStripMain.TabIndex = 0;
+            // 
+            // ToolStripMain
+            // 
+            this.ToolStripMain.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.ToolStripMain.Location = new System.Drawing.Point(0, 24);
+            this.ToolStripMain.Name = "ToolStripMain";
+            this.ToolStripMain.Size = new System.Drawing.Size(484, 25);
+            this.ToolStripMain.TabIndex = 1;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(484, 262);
+            this.Controls.Add(this.ToolStripMain);
+            this.Controls.Add(this.MenuStripMain);
+            this.MainMenuStrip = this.MenuStripMain;
             this.Name = "MainForm";
             this.Text = "IMP";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.OnMainFormClosed);
@@ -42,6 +62,9 @@
             this.ResumeLayout(false);
 
         }
+
+        private System.Windows.Forms.MenuStrip MenuStripMain;
+        private System.Windows.Forms.ToolStrip ToolStripMain;
 
         #endregion
     }
